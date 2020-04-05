14879692838ad8613c5e88bd91edad3a30eec8a2
Subrat <36433481+subrat82@users.noreply.github.com>
2020-04-04 20:40:07 -0700

    Update parameterised-shell-script.sh

--- parameterised-shell-script.sh
+++ parameterised-shell-script.sh
@@ -6,33 +6,33 @@
 
 if [ $1 != " " ] ; then
     kerberos_location="$1"
-    sed -ie "s%<<kerberoslocation>>%${kerberos_location}%g" config.json
+    sed -ie "s%<<base_url>>%${kerberos_location}%g" config.json
 else
   echo "Need Kerberos Location argument to continue"
   exit 1
 fi
 
 
-if [ $2 != " " ] ; then
-    kerberos_location="$2"
-    sed -ie "s%<<kerberoslocation>>%${kerberos_username}%g" config.json
-else
-  echo "Need Kerberos Location argument to continue"
-  exit 1
-fi
+# if [ $2 != " " ] ; then
+#     kerberos_location="$2"
+#     sed -ie "s%<<kerberoslocation>>%${kerberos_username}%g" config.json
+# else
+#   echo "Need Kerberos Location argument to continue"
+#   exit 1
+# fi
 
 
-if [ $3 != " " ] ; then
-    kerberos_location="$3"
-    sed -ie "s%<<kerberoslocation>>%${kerberos_password}%g" config.json
-else
-  echo "Need Kerberos Location argument to continue"
-  exit 1
-fi
+# if [ $3 != " " ] ; then
+#     kerberos_location="$3"
+#     sed -ie "s%<<kerberoslocation>>%${kerberos_password}%g" config.json
+# else
+#   echo "Need Kerberos Location argument to continue"
+#   exit 1
+# fi
 
-echo "$kerberos_location"
-echo "$kerberos_username"
-echo "$kerberos_password"
+# echo "$kerberos_location"
+# echo "$kerberos_username"
+# echo "$kerberos_password"
 
 
 
